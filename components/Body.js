01203e2a0cabaf3193fdd705d99f9e31fcf207c5
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-10-12 22:38:38 +0600

    map card and click is working

--- components/Body.js
+++ components/Body.js
@@ -18,19 +18,109 @@ const Body = () => {
   const [pageIsMounted, setPageIsMounted] = useState(false);
   mapboxgl.accessToken =
     "pk.eyJ1Ijoid2FubmFkYyIsImEiOiJjazBja2M1ZzYwM2lnM2dvM3o1bmF1dmV6In0.50nuNnApjrJYkMfR2AUpXA";
- 
+
   useEffect(() => {
-      const map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/light-v10",
-        center: [-77.034084, 38.909671],
-        zoom: 13,
-        scrollZoom: false,
-      });
-      // Add zoom and rotation controls to the map.
-      map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
-      setMap(map);
+    setPageIsMounted(true);
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [-77.034084, 38.909671],
+      zoom: 13,
+      scrollZoom: false,
+    });
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+    setMap(map);
   }, []);
+  /**
+   * Wait until the map loads to make changes to the map.
+   */
+  /**
+   * Assign a unique id to each store. You'll use this `id`
+   * later to associate each point on the map with a listing
+   * in the sidebar.
+   */
+  stores.features.forEach((store, i) => {
+    store.properties.id = i;
+  });
+  useEffect(() => {
+    if (pageIsMounted && stores) {
+      Map.on("load", () => {
+        Map.addSource("places", {
+          type: "geojson",
+          data: stores,
+        });
+        addMarkers();
+      });
+    }
+  });
+
+  /**
+   * Add a marker to the map for every store listing.
+   **/
+  function addMarkers() {
+    /* For each feature in the GeoJSON object above: */
+    for (const marker of stores.features) {
+      /* Create a div element for the marker. */
+      const el = document.createElement("div");
+      /* Assign a unique `id` to the marker. */
+      el.id = `marker-${marker.properties.id}`;
+      /* Assign the `marker` class to each marker for styling. */
+      el.className = "marker";
+
+      /**
+       * Create a marker using the div element
+       * defined above and add it to the map.
+       **/
+      new mapboxgl.Marker(el, { offset: [0, -23] })
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(Map);
+
+      /**
+       * Listen to the element and when it is clicked, do three things:
+       * 1. Fly to the point
+       * 2. Close all other popups and display popup for clicked store
+       * 3. Highlight listing in sidebar (and remove highlight for all other listings)
+       **/
+      el.addEventListener("click", (e) => {
+        /* Fly to the point */
+        flyToStore(marker);
+        /* Close all other popups and display popup for clicked store */
+        createPopUp(marker);
+        /* Highlight listing in sidebar */
+        const activeItem = document.getElementsByClassName("active");
+        e.stopPropagation();
+        if (activeItem[0]) {
+          activeItem[0].classList.remove("active");
+        }
+      });
+    }
+  }
+
+  /**
+   * Use Mapbox GL JS's `flyTo` to move the camera smoothly
+   * a given center point.
+   **/
+  function flyToStore(currentFeature) {
+    Map.flyTo({
+      center: currentFeature.geometry.coordinates,
+      zoom: 15,
+    });
+  }
+
+  /**
+   * Create a Mapbox GL JS `Popup`.
+   **/
+  function createPopUp(currentFeature) {
+    const popUps = document.getElementsByClassName("mapboxgl-popup");
+    if (popUps[0]) popUps[0].remove();
+    const popup = new mapboxgl.Popup({ closeOnClick: false })
+      .setLngLat(currentFeature.geometry.coordinates)
+      .setHTML(
+        `<h3>Sweetgreen</h3><h4>${currentFeature.properties.address}</h4>`
+      )
+      .addTo(Map);
+  }
 
   return (
     <Div>
